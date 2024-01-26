0a40093d93e7945921f8d324b93442a91d631bc4
SWATHINKTK <swathinktk10@gmail.com>
2024-01-26 11:47:33 +0530

    genre view component design completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Banner from './components/banner/Banner';
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
+import Genre from './components/genre_component/Genre';
 
 function App() {
   
@@ -9,6 +10,12 @@ function App() {
     <>
       <Navbar/>
       <Banner/>
+      <Genre/>
+      <Genre/>
+      <Genre/>
+      <Genre/>
+      <Genre/>
+      <Genre/>
       <Footer/>
     </>
   )
--- src/components/genre_component/Genre.jsx
+++ src/components/genre_component/Genre.jsx
@@ -3,7 +3,19 @@ import './genre.css'
 function Genre(){
     return(
         <div className="genre-main-div">
-
+            <h1 className='genre-heading'>Trending</h1>
+            <div className='genre-image-view'>
+                <img src="/public/images/Money.jpeg" alt="genre image Loading" />
+                <img src="/public/images/Money.jpeg" alt="genre image Loading" />
+                <img src="/public/images/Money.jpeg" alt="genre image Loading" />
+                <img src="/public/images/Money.jpeg" alt="genre image Loading" />
+                <img src="/public/images/Money.jpeg" alt="genre image Loading" />
+                <img src="/public/images/Money.jpeg" alt="genre image Loading" />
+                <img src="/public/images/Money.jpeg" alt="genre image Loading" />
+                <img src="/public/images/Money.jpeg" alt="genre image Loading" />
+                <img src="/public/images/Money.jpeg" alt="genre image Loading" />
+                <img src="/public/images/Money.jpeg" alt="genre image Loading" />
+            </div>
         </div>
     )
 }
